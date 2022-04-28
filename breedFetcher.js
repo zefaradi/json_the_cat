95c6a0b358c6bdfbd323add9461033c0f92af121
Adib Zefar <adibzefar@gmail.com>
2022-04-28 00:21:31 -0400

    refactor the code include callback from index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,18 @@
 const request = require('request');
 
-const fetchDesccription = breed => {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
-    if (response.statusCode !== 200) {
-      console.log(`Sorry! Something went wrong: ${body}'`);
-      return;
+    if (error) {
+      callback(error);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log('No breed name found');
+      callback(error);
     } else {
-      console.log(data[0].description);
+      callback(error, data[0].description);
     }
   });
 };
 
-const input = process.argv.slice(2);
-const breed = input[0];
-
-fetchDesccription(breed);
+module.exports = { fetchBreedDescription };
